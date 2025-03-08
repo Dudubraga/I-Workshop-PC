f8b9659980d2d2187f2cd61e71034aa9776a29fa
juliaavilelaa <juliavilela3@gmail.com>
2025-03-07 23:19:30 -0300

    Back to High School Physics

---
+++ Logica_Matematica_Condicionais/Questoes Aula/BackToHighSchoolPhysics.cpp
@@ -0,0 +1,16 @@
+/* 
+Back to High School Physics - UVA (https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=1012)
+Júlia
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    int v, t;
+    
+    while (cin >> v >> t) {
+        cout << v*(2*t) << endl;
+    }
+
+    return 0;
+}
