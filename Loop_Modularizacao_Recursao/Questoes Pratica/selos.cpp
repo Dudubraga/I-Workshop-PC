e1535fc00b5b46f4499a40813478a026c77e86d3
juliaavilelaa <juliavilela3@gmail.com>
2025-03-09 15:31:35 -0300

    Movendo questões

---
+++ Loop_Modularizacao_Recursao/Questoes Pratica/selos.cpp
@@ -0,0 +1,28 @@
+/* 
+Nome da Questão: SELOS11 SPOJ: https://br.spoj.com/problems/SELOS11/
+Nome de quem resolveu: Isabela Medeiros
+*/
+
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    long long int r, n;
+    int cont = 0;
+    cin >> n;
+    for (int i = 1; i <= sqrt(n); i++){
+        r = n % i;
+        if (r == 0){
+           cont++;      
+        }    
+    }
+    if (n < 4 || cont < 2){
+       cout <<"N";
+    }
+    else if (cont >= 2){
+        cout<<"S";         
+    }
+
+    return  0;
+}
