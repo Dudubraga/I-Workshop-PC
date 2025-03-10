f2fc2b973c7f07589af4fb9ba0d8e0973603260d
Maria Luiza Lima <132663650+mlrlima@users.noreply.github.com>
2025-03-09 22:34:49 -0300

    quadrados

---
+++ Logica_Matematica_Condicionais/Questoes Pratica/quadrados.cpp
@@ -0,0 +1,17 @@
+/*
+Quadrados - SPOJ (https://br.spoj.com/problems/QUADRAD2/)
+Maria Luiza
+*/
+#include <iostream>
+using namespace std;
+ 
+int main() {
+	
+	int n;
+	cin>>n;
+	int q;
+	q=n*n;
+	cout<<q;
+ 
+	return 0;
+} 
