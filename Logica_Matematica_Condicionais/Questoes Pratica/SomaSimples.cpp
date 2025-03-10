776cb45c027c4a324e7841d80ccd6596087bdae3
Maria Luiza Lima <132663650+mlrlima@users.noreply.github.com>
2025-03-09 22:41:07 -0300

    Soma Simples

---
+++ Logica_Matematica_Condicionais/Questoes Pratica/SomaSimples.cpp
@@ -0,0 +1,15 @@
+/*
+Soma Simples - Beecrowd (https://judge.beecrowd.com/pt/problems/view/1003)
+Maria Luiza
+*/
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int a,b;
+    scanf("%d %d",&a,&b);
+    printf("SOMA = %d\n",a+b);
+    
+    return 0;
+}
