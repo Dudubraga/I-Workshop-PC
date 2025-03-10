2ee6dd7069a7f940c3ae1374a8de87b1cf193804
Maria Luiza Lima <132663650+mlrlima@users.noreply.github.com>
2025-03-09 22:44:03 -0300

    Área do círculo

---
+++ Logica_Matematica_Condicionais/Questoes Pratica/AreaDoCirculo.cpp
@@ -0,0 +1,16 @@
+/*
+Área do círculo - Beecrowd (https://judge.beecrowd.com/pt/problems/view/1002)
+Maria Luiza
+*/
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    double r,a;
+    scanf("%lf",&r);
+    a=r*r*3.14159;
+    printf("A=%.4lf\n",a);
+    
+    return 0;
+}
