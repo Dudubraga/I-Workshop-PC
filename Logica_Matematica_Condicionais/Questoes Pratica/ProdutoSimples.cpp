026c59fe8d0c26014d93922f5141f36a4dbb5570
Maria Luiza Lima <132663650+mlrlima@users.noreply.github.com>
2025-03-09 22:39:30 -0300

    Produto Simples

---
+++ Logica_Matematica_Condicionais/Questoes Pratica/ProdutoSimples.cpp
@@ -0,0 +1,15 @@
+/*
+Produto Simples - Beecrowd (https://judge.beecrowd.com/pt/problems/view/1004)
+Maria Luiza
+*/
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int a,b;
+    scanf("%d %d",&a,&b);
+    printf("PROD = %d\n",a*b);
+    
+    return 0;
+}
