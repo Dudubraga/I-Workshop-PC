55f9bcb6df250e04e97c51c13ba79fc4b1ba056e
Maria Luiza Lima <132663650+mlrlima@users.noreply.github.com>
2025-03-09 22:33:04 -0300

    pneu

---
+++ Logica_Matematica_Condicionais/Questoes Pratica/pneu.cpp
@@ -0,0 +1,17 @@
+/* 
+Pneu - SPOJ (https://br.spoj.com/problems/JPNEU/) 
+Maria Luiza
+*/
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+	
+	int n, m;
+	cin>>n;
+	cin>>m;
+	int d=n-m;
+	cout<<d;
+	
+	return 0;
+} 
